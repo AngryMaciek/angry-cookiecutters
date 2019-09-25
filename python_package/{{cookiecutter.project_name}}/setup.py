f4a2a8bd463f3942bb7e933f975f6e0af2ec0d39
Maciej Bak <wsciekly.maciek@gmail.com>
2019-09-25 23:38:31 +0200

    python pkg: added setup.py

---
+++ python_package/{{cookiecutter.project_name}}/setup.py
@@ -0,0 +1,26 @@
+##############################################################################
+#
+#   {{cookiecutter.package_name}}
+#
+#   Setup script for the package.
+#
+#   AUTHOR: {{cookiecutter.author}}
+#   AFFILIATION: {{cookiecutter.affiliation}}
+#   CONTACT: {{cookiecutter.contact}}
+#   CREATED: {{cookiecutter.date_created}}
+#   LICENSE: {{cookiecutter.license}}
+#   USAGE: ...
+#
+##############################################################################
+
+from setuptools import setup
+
+setup(name="{{cookiecutter.package_name}}",
+	version="0.0.0.9000",
+	description="",
+	url="",
+	author="{{cookiecutter.author}}",
+	author_email="{{cookiecutter.contact}}",
+	license="{{cookiecutter.license}}",
+	packages=["{{cookiecutter.package_name}}"],
+	zip_safe=False)
