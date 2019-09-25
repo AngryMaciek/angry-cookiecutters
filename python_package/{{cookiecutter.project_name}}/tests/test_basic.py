400693f00d0534add30e28deb0d041dd8f81b209
Maciej Bak <wsciekly.maciek@gmail.com>
2019-09-26 00:44:47 +0200

    python pkg: added pytest testfile and exceptions file

---
+++ python_package/{{cookiecutter.project_name}}/tests/test_basic.py
@@ -0,0 +1,33 @@
+##############################################################################
+#
+#   {{cookiecutter.package_name}}
+#
+#   Basic package tests.
+#
+#   AUTHOR: {{cookiecutter.author}}
+#   AFFILIATION: {{cookiecutter.affiliation}}
+#   CONTACT: {{cookiecutter.contact}}
+#   CREATED: {{cookiecutter.date_created}}
+#   LICENSE: {{cookiecutter.license}}
+#   USAGE: pytest test_basic.py
+#
+##############################################################################
+
+# import the testing framework
+import pytest
+
+# import the package
+import {{cookiecutter.package_name}}
+
+##############################################################################
+
+def test_simple_load():
+	"""Tests if the package is functional."""
+	result = {{cookiecutter.module_name}}.f(True)
+	assert result
+
+def test_exception():
+	"""Tests if the exception is raised correctly."""
+	with pytest.raises({{cookiecutter.module_name}}.\
+		exceptions.SomethingWentWrong):
+			result = {{cookiecutter.module_name}}.f(False)
---
+++ python_package/{{cookiecutter.project_name}}/{{cookiecutter.package_name}}/exceptions.py
@@ -0,0 +1,17 @@
+##############################################################################
+#
+#   {{cookiecutter.package_name}}
+#
+#   Exceptions for the package.
+#
+#   AUTHOR: {{cookiecutter.author}}
+#   AFFILIATION: {{cookiecutter.affiliation}}
+#   CONTACT: {{cookiecutter.contact}}
+#   CREATED: {{cookiecutter.date_created}}
+#   LICENSE: {{cookiecutter.license}}
+#
+##############################################################################
+
+class SomethingWentWrong(Exception):
+	"""Raised when something went wrong."""
+	pass
